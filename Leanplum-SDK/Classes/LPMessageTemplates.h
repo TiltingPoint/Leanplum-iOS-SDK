acad4c09e85836ac94432d9ddaef4a1b615d1de5
Santiago Castaneda Munoz <santiagocm98@hotmail.com>
2019-09-24 16:24:24 -0400

    Removing Unused Code

--- Leanplum-SDK/Classes/LPMessageTemplates.h
+++ Leanplum-SDK/Classes/LPMessageTemplates.h
@@ -35,7 +35,6 @@
 @interface LPMessageTemplatesClass : NSObject <UIAlertViewDelegate, WKNavigationDelegate>
 
 + (LPMessageTemplatesClass *)sharedTemplates;
-+ (BOOL) triggerEmbeddedUrl;
 
 - (void)disableAskToAsk;
 - (void)refreshPushPermissions;
